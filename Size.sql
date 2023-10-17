ed933389b9bfcbc409caa20fb7192b4a07a44a72
MarcusBrobert <marcus.brobert@gmail.com>
2023-10-17 17:41:34 +0200

    Add Product.sql

--- Size.sql
+++ Size.sql
@@ -5,7 +5,7 @@ WHERE storeId = '96121ae8-1808-4a2b-9b45-3a45d148675e';
 -- Insert testdata into Size table
 INSERT INTO Size (name, description, storeId)
 VALUES
-('Small', 'Small size', '96121ae8-1808-4a2b-9b45-3a45d148675e'),
-('Medium', 'Medium size', '96121ae8-1808-4a2b-9b45-3a45d148675e'),
-('Large', 'Large size', '96121ae8-1808-4a2b-9b45-3a45d148675e'),
+('S', 'Small size', '96121ae8-1808-4a2b-9b45-3a45d148675e'),
+('M', 'Medium size', '96121ae8-1808-4a2b-9b45-3a45d148675e'),
+('L', 'Large size', '96121ae8-1808-4a2b-9b45-3a45d148675e'),
 ('XL', 'Extra Large size', '96121ae8-1808-4a2b-9b45-3a45d148675e');
